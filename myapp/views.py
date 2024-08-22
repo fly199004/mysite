28a1ea70509f938e9c4a20b9ad11c7d3311a32aa
fly199004 <414562979@qq.com>
2024-08-22 08:37:57 +0800

    完成文章网页设计，将md转化成网页渲染，图片正常显示，右边显示文章目录

--- myapp/views.py
+++ myapp/views.py
@@ -10,6 +10,7 @@ from django.http import JsonResponse
 from pymongo import MongoClient
 from django.conf import settings
 from bson import ObjectId
+import markdown,re
 # Create your views here.
 
 def index(request):
@@ -17,6 +18,14 @@ def index(request):
 
 def test(request):
     return render(request, 'test.html')
+
+# 解析文章内容中的图床 URL 并将它们转换为 <img> 标签
+def process_image_urls(content):
+    # 使用正则表达式找到所有图床URL并替换为 <img> 标签
+    pattern = r'!\[(.*?)\]\((.*?)\)'  # 匹配 Markdown 格式的图片
+    replacement = r'<img src="\2" alt="\1">'
+    processed_content = re.sub(pattern, replacement, content)
+    return processed_content
 
 
 def articles(request, article_id=None):
@@ -39,11 +48,26 @@ def articles(request, article_id=None):
         article = db.articles.find_one({'_id': ObjectId(article_id)})
         if article:
             selected_article = {
-                'title': article.get('title'),
-                'content': article.get('content'),
+                'title': article.get('title'),                                                      
                 'author': article.get('author'),
                 'createdAt': article.get('createdAt').strftime('%Y-%m-%d %H:%M:%S') if article.get('createdAt') else ''
             }
+            # 1. 获取文章内容
+            content = article.get('content', '')
+            # 2. 处理图床 URL
+            processed_content = process_image_urls(content)
+            # 3. 使用 toc 生成目录和 HTML 内容
+            md = markdown.Markdown(extensions=['toc', 'extra'])
+            html_content = md.convert(processed_content)
+            toc_content = md.toc  # 获取目录内容
+            # 将处理后的 HTML 内容赋值给 selected_article 的 'content' 字段
+            selected_article['content'] = html_content
+            # 将目录内容赋值给 selected_article 的 'toc' 字段
+            selected_article['toc'] = toc_content
+           
+           
+
+           
 
     context = {
         'articles': article_list,
